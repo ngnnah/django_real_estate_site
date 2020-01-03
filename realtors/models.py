7bc08cb25860648d2853b8b2cbbe8fdd03b138d2
Nhat Nguyen <nhatnguyen@nhats-mbp.lan>
2020-01-03 13:10:50 -0500

    postgres, models, superuser, upload/create db, modify admin area

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    is_mvp = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
