b7101003a72efb196341f042c9e4659858e30e71
Nhat Nguyen <nhatnguyen@nhats-mbp.lan>
2020-01-04 01:00:09 -0500

    customize Admin area, dynamize UI

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-
 from .models import Listing
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',) #dont forget the comma
+    list_editable = ('is_published',) #dont forget the comma
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+admin.site.register(Listing, ListingAdmin)
