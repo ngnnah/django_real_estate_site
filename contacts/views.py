47eb72304ef6738fb593129785fd83a9a7ef6811
Nhat Nguyen <nhatnguyen@nhats-mbp.lan>
2020-01-06 15:19:34 -0500

    disable email

--- contacts/views.py
+++ contacts/views.py
@@ -29,14 +29,14 @@ def contact(request):
 
         contact.save()
 
-        #send email
-        send_mail("Property Listing Inquiry",
-            "There has been an inquiry for " + listing + '. Sign into the admin panel for more info.',
-            'jeff@amazon.com',
-            [realtor_email, 'untoturtle@gmail.com'],
-            fail_silently=False
+        # #send email
+        # send_mail("Property Listing Inquiry",
+        #     "There has been an inquiry for " + listing + '. Sign into the admin panel for more info.',
+        #     'jeff@amazon.com',
+        #     [realtor_email, 'untoturtle@gmail.com'],
+        #     fail_silently=False
             
-        )
+        # )
 
         messages.success(request, 
             'Your request has been submitted, a realtor will get back to you soon')
