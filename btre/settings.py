db700b264dc381c958d26d20f031e8cc3517ea08
Nhat Nguyen <nhatnguyen@nhats-mbp.lan>
2020-01-06 00:07:07 -0500

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'moonthief11235@gmail.com'
 EMAIL_HOST_PASSWORD = 'AvaLe2016'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
